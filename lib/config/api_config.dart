a1a5a042afe8f61a9ab87f34a99f19b44fd4da5a
Muhammad Shidqi Hanif <shidqi24@mhs.usk.ac.id>
2026-07-23 17:03:50 +0700

    feat: tambah API config

---
+++ lib/config/api_config.dart
@@ -0,0 +1,18 @@
+class ApiConfig {
+  static const String baseUrl = 'http://192.168.56.1:8000/api';
+
+  // Endpoints Auth
+  static const String register = '/register';
+  static const String login = '/login';
+  static const String logout = '/logout';
+  static const String user = '/user';
+
+  // Endpoints Muhasabah
+  static const String muhasabah = '/muhasabah';
+
+  // Endpoints Tracker
+  static const String tracker = '/tracker';
+
+  // Endpoints Dashboard
+  static const String dashboard = '/dashboard';
+}
